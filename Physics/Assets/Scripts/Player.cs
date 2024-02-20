2b12a273e6976f8eb7021885f32ea053fbe5b1a6
Konrad-Haddadi <haddadikonrad@gmail.com>
2024-02-20 12:26:33 +1100

    Made jump bar

--- Physics/Assets/Scripts/Player.cs
+++ Physics/Assets/Scripts/Player.cs
@@ -1,13 +1,19 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Animator))]
 
 public class Player : MonoBehaviour
 {
+    private float jumpPowerMax;
+    private bool up;
+
     public float forwardSpeed = 160f;
     public float rotationSpeed = 16f;
     public float pushPower = 2f;
     public float jumpPower = 2f;
+    
 
     [HideInInspector] public Animator animator = null;
     public Rigidbody rb;
@@ -17,44 +23,72 @@ public class Player : MonoBehaviour
     public bool isJumping = false;
     public float jumpTimer = 0;
 
+    public Canvas canvas = null;
+    public Image bar = null; 
+
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         capCollider = GetComponent<CapsuleCollider>();
         rb = GetComponent<Rigidbody>();
+        jumpPowerMax = jumpPower;
+        up = false;
+        canvas.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-
+        
         animator.SetFloat("Speed", horizontal);
 
         if(!isJumping)
             transform.position += transform.forward * horizontal * forwardSpeed * Time.deltaTime;
 
+        if(Input.GetKey(KeyCode.Space))
+        {
+            if(up)
+            {
+                jumpPower += Time.deltaTime * 10;
 
-        if (Input.GetKeyDown(KeyCode.Space))
+                if (jumpPower >= jumpPowerMax)
+                    up = false;
+            }
+            else
+            {
+                jumpPower -= Time.deltaTime * 10;
+
+                if (jumpPower <= 0)
+                    up = true;
+            }
+
+            if(!isJumping)            
+                canvas.enabled = true;
+
+            bar.fillAmount = jumpPower / jumpPowerMax;
+        }
+
+        if(Input.GetKeyUp(KeyCode.Space))
         {
             isJumping = !isJumping;
-            
+            canvas.enabled = false;
+
             capCollider.enabled = !isJumping;
             rb.isKinematic = isJumping;
 
             if (isJumping)
             {
                 animator.enabled = !isJumping;
-                hips.velocity += (Vector3.up + transform.forward * horizontal) * (jumpPower * 5);            
+                hips.velocity += ((transform.up * (jumpPower * 5)) + (transform.forward * horizontal * jumpPowerMax));
             }
             else
             {
                 transform.position = new Vector3(hips.transform.position.x, hips.transform.position.y, transform.position.z);
                 animator.enabled = !isJumping;
-            }            
-        }
-
+            }
+        }  
 
         animator.SetBool("Fall", isJumping);
     }
