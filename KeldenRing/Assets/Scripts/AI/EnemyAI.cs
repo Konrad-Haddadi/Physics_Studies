56be8a742de56e6fd3845fd4c2fec3ab01079e95
Konrad-Haddadi <haddadikonrad@gmail.com>
2024-02-27 16:13:54 +1100

    Line of sight check

--- KeldenRing/Assets/Scripts/AI/EnemyAI.cs
+++ KeldenRing/Assets/Scripts/AI/EnemyAI.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.LowLevel;
 
 public class EnemyAI : MonoBehaviour
 {
@@ -23,26 +24,54 @@ public class EnemyAI : MonoBehaviour
         if(player)
         {
             if (!agent.hasPath)
-                agent.SetDestination(player.transform.position);
+            {
+                if(LookForPlayer())
+                    agent.SetDestination(player.transform.position);
+            }
 
-            float dist = Vector3.Distance(player.transform.position, agent.destination);
+            float dist = Vector3.Distance(player.transform.position, agent.pathEndPosition);
 
             if(dist > 2)
-                agent.SetDestination(player.transform.position);
+            {
+                if(LookForPlayer())
+                    agent.SetDestination(player.transform.position);
+                
+            }
+
+            Debug.DrawRay(transform.position, (player.transform.position - transform.position) * 5);
+        }        
+    }
+
+    private bool LookForPlayer()
+    {
+        RaycastHit hit;
+        Ray raycast = new Ray(transform.position, player.transform.position + Vector3.up - transform.position);
+
+        if (Physics.Raycast(raycast, out hit, visionDist))
+        {
+            if (hit.transform.GetComponent<PlayerComponents>())
+                return true;
         }
+
+        return false;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (player)
             return;
-        
-        player = other.GetComponentInParent<PlayerComponents>();
+
+        PlayerComponents playerComp = other.GetComponent<PlayerComponents>();
+
+        if(playerComp)
+        {
+            player = playerComp;
+        }      
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if(other.GetComponentInParent<PlayerComponents>())
+        if(other.GetComponent<PlayerComponents>())
         {
             player = null;
         }
